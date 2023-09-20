adf3c276f9531995a5ad5e861663be2ba90c9074
Tom Seufert <seufertt@seuferttV07RD.vmware.com>
2023-09-20 07:41:41 -0400

    random change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 
 <section>
 	<h1>
-		<span class="welcome">
+		<span class="welcome ">
 			<picture>
 				<source srcset={welcome} type="image/webp" />
 				<img src={welcome_fallback} alt="Welcome" />
